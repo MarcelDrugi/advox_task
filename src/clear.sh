6ad4b4fddca81ff2bdbbf960f0b8f3622b18d2b6
Piotr Mazur <piotr.a.mazur@wp.pl>
2023-01-26 14:38:28 +0100

    Magento skeleton

---
+++ src/clear.sh
@@ -0,0 +1,1 @@
+chmod -R 777 var/ pub/ generated/ && rm -rf var/cache/* pub/static/* generated/* && php bin/magento setup:upgrade && php bin/magento setup:di:compile && php bin/magento setup:static-content:deploy -f && php bin/magento indexer:reindex && php bin/magento cache:flush && chmod -R 777 var/ pub/ generated/
---
+++ src/install.sh
@@ -0,0 +1,1 @@
+bin/magento setup:config:set --backend-frontname=admin --db-host=mysql --db-name=mydatabase --db-user=myuser --db-password=test123 && bin/magento setup:install && bin/magento admin:user:create --admin-user='admin' --admin-password='test123' --admin-email='admin@admin.com' --admin-firstname='Admin' --admin-lastname='Admin'
